d070011b198700c3eab13d99239dac0ca08963a6
MikePHaynes <86630042+MikePHaynes@users.noreply.github.com>
2022-08-03 16:31:13 -0700

    Implemented stack and node classes.

---
+++ Rack-OGame/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rack_OGame
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node? Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
---
+++ Rack-OGame/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rack_OGame
+{
+    public class Stack
+    {
+        public int Size { get; set; }
+        public Node? Top { get; set; }
+
+        public Stack()
+        {
+            Size = 0;
+            Top = null;
+        }
+
+        public void Push(Node node)
+        {
+            node.Next = Top;
+            Top = node;
+            Size++;
+        }
+
+        public Node? Pop()
+        {
+            if (Size == 0) return null;
+
+            Node? old = Top;
+            Top = Top.Next;
+            Size--;
+            old.Next = null;
+            return old;
+        }
+    }
+}
